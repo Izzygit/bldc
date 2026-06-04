7bf6af59f8336759aa9fcbfcd3490f594a406415
Izzygit <126716104+Izzygit@users.noreply.github.com>
2026-06-04 15:06:09 -0700

    Update digital_filter.c

--- util/digital_filter.c
+++ util/digital_filter.c
@@ -250,13 +250,20 @@ void biquad_config(Biquad *biquad, BiquadType type, float Fc) {
 		biquad->a0 = K * K * norm;
 		biquad->a1 = 2 * biquad->a0;
 		biquad->a2 = biquad->a0;
+		biquad->b1 = 2 * (K * K - 1) * norm;
 	}
 	else if (type == BQ_HIGHPASS) {
 		biquad->a0 = 1 * norm;
 		biquad->a1 = -2 * biquad->a0;
 		biquad->a2 = biquad->a0;
+		biquad->b1 = 2 * (K * K - 1) * norm;
+	} 
+	else if (type == BQ_NOTCH) {
+        biquad->a0 = (1 + K * K) * norm;
+        biquad->a1 = 2 * (K * K - 1) * norm;
+        biquad->a2 =  biquad->a0;
+        biquad->b1 =  biquad->a1;
 	}
-	biquad->b1 = 2 * (K * K - 1) * norm;
 	biquad->b2 = (1 - K / Q + K * K) * norm;
 }
 void biquad_reset(Biquad *biquad) {
